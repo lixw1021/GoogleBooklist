1052ed5d7f2594f067d24085a261e9da2b5760d6
xianwei li <xil148@pitt.edu>
2017-07-26 18:53:02 -0400

    Created UI

---
+++ app/src/main/java/com/example/xianweili/googlebooklist/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.xianweili.googlebooklist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xianwei li on 7/26/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
