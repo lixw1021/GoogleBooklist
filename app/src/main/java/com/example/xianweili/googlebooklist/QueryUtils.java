2cbd54724863560ca2a153010cafd1cc4a5e9394
xianwei li <xil148@pitt.edu>
2017-07-27 18:54:35 -0400

    fetch data through url and polished UI

--- app/src/main/java/com/example/xianweili/googlebooklist/QueryUtils.java
+++ app/src/main/java/com/example/xianweili/googlebooklist/QueryUtils.java
@@ -1,5 +1,6 @@
 package com.example.xianweili.googlebooklist;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
@@ -8,12 +9,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.xianweili.googlebooklist.MainActivity.LOG_TAG;
 
 /**
  * Created by xianwei li on 7/26/2017.
@@ -24,9 +32,82 @@ public final class QueryUtils {
     private QueryUtils() {
     }
 
+    public static List<Book> fetchBookDate(String urlString) {
+        URL url = createUrl(urlString);
+        String jsonResponse = makeHttpRequest(url);
+        return extractBooks(jsonResponse);
+    }
+
+    private static URL createUrl(String urlString){
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "problem with create URL", e);
+            return null;
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) {
+        InputStream inputStream = null;
+        HttpURLConnection connection = null;
+        String jsonResponse = "";
+
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = connection.getInputStream();
+                jsonResponse = getJsonFromStream(inputStream);
+            } else {
+                throw new IOException("HTTP error code: " + connection.getResponseCode());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "problem with makeHTTPrequest",e);
+                }
+            }
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String getJsonFromStream(InputStream inputStream) throws IOException {
+        StringBuilder result = new StringBuilder();
+
+        if (inputStream != null) {
+            Reader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                result.append(line);
+                line = reader.readLine();
+            }
+        }
+        return result.toString();
+    }
 
 
-    public static ArrayList<Book> extractBooks (String BooksJSONString) {
+    private static ArrayList<Book> extractBooks (String BooksJSONString) {
         if (BooksJSONString == null) {
             return null;
         }
@@ -61,7 +142,7 @@ public final class QueryUtils {
         return books;
     }
 
-    public static Bitmap getBitmapFromUrl(String imageLink) {
+    private static Bitmap getBitmapFromUrl(String imageLink) {
         try {
             URL url = new URL(imageLink);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
